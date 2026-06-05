eb531d99fbd67bfb9db8b878249b3ecb2fa2e3ce
diawsz67 <sneine.pepe666@atomicmail.io>
2026-06-05 17:30:14 +0300

    Update adapter.py

--- pattern_lz/adapter.py
+++ pattern_lz/adapter.py
@@ -1,15 +1,25 @@
-import copy
+class Babushka:
+    def polite_speech(self, text:str):
+        print('Люблю тебя мой внучек,очень рада тебя видеть')
 
-class Face:
-    def __init__(self, kolichectvoeyes, kolichestvovolosbrows, colorcheeks):
-        self.kolichectvoeyes = kolichectvoeyes
-        self.kolichestvovolosbrows = kolichestvovolosbrows
-        self.colorcheeks = colorcheeks
 
-    def clone(self):
-        return copy.deepcopy(self)
+class Child:
+    def unpolite_speech(self):
+        return 'Йоу, здарова, бабка, у меня все ништяк 67'
 
-face1 = Face(15,7,'зеленый')
-copyface = face1.clone()
 
-print(copyface.colorcheeks)
+
+class PoliteAdapter:
+    def __init__(self, gangster: Child ):
+        self.gangster = gangster
+
+    def translate_to_babka(self, babulik: Babushka):
+        slang = self.gangster.unpolite_speech()
+        polite_text = 'Привет, дорогая бабушка, у меня все очень хорошо'
+        babulik.polite_speech(polite_text)
+
+babulik = Babushka()
+gangster = Child()
+
+perevod = PoliteAdapter(gangster)
+perevod.translate_to_babka(babulik)
