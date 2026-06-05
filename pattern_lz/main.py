c4af333e5d7224a177e18313027feb0f103a5a97
diawsz67 <sneine.pepe666@atomicmail.io>
2026-06-05 18:40:02 +0300

    Update main.py

--- pattern_lz/main.py
+++ pattern_lz/main.py
@@ -1,26 +1,8 @@
-class Babushka:
-    def polite_speech(self, text:str):
-        print('Люблю тебя мой внучек,очень рада тебя видеть')
+from posrednik import *
+from prototip import *
+from adapter import *
 
-
-class Child:
-    def unpolite_speech(self):
-        return 'Йоу, здарова, бабка, у меня все ништяк 67'
-
-
-
-class PoliteAdapter:
-    def __init__(self, gangster: Child ):
-        self.gangster = gangster
-
-    def translate_to_babka(self, babulik: Babushka):
-        slang = self.gangster.unpolite_speech()
-        polite_text = 'Привет, дорогая бабушка, у меня все очень хорошо'
-        babulik.polite_speech(polite_text)
-
-babulik = Babushka()
-gangster = Child()
-
-perevod = PoliteAdapter(gangster)
-perevod.translate_to_babka(babulik)
-
+if __name__ == "__main__":
+    print("\nВсе три файла выполнены")
+    
+    
